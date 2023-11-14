e8c1c59a7240cf434e4b36f7587aa79b1b68aea4
sunny park <sunnyparkk@gmail.com>
2023-11-13 23:12:54 -0500

    Add selector

---
+++ src/state/selectors/TotalTodos.ts
@@ -0,0 +1,10 @@
+import { selector } from "recoil";
+import { todoState } from "../atoms/TodoState";
+
+export const totalTodos = selector({
+	key: "totalTodos",
+	get: ({ get }) => {
+		const todos = get(todoState);
+		return todos.length;
+	},
+});
