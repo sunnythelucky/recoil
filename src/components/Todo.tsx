5e18b59d5929da59227b3dc31ae17c25e35f98aa
sunny park <sunnyparkk@gmail.com>
2023-11-13 23:13:55 -0500

    Add todo

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,3 +1,25 @@
-export default function Todo() {
-	return <div>Todo</div>;
+import { todoState } from "../state/atoms/TodoState";
+import { useSetRecoilState } from "recoil";
+import { Todo } from "./Todos";
+
+type TodoProps = {
+	text: string;
+	id: string;
+};
+
+export default function Todo({ text, id }: TodoProps) {
+	const setTodos = useSetRecoilState(todoState);
+
+	const deleteTodo = () => {
+		setTodos((prevTodos: Todo[]) => prevTodos.filter((todo) => todo.id !== id));
+	};
+
+	return (
+		<div className="flex flex-row space-between gap-2">
+			<p className="w-4/5">{text}</p>
+			<button className="w-1/5" onClick={deleteTodo}>
+				X
+			</button>
+		</div>
+	);
 }
