f8184017f9abb49bfa2f822ef595e739d2926ddf
sunny park <sunnyparkk@gmail.com>
2023-11-13 23:14:00 -0500

    Add todo to Todos

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import { todoState } from "../state/atoms/TodoState";
 import { useRecoilState } from "recoil";
 import { v4 as uuidv4 } from "uuid";
+import { todoState } from "../state/atoms/TodoState";
+import Todo from "./Todo";
 
 export type Todo = {
 	id: string;
@@ -18,7 +19,7 @@ export default function Todos() {
 
 	const addTodo = () => {
 		if (inputText.length > 0) {
-			setTodos([...todos, { id: uuidv4(), text: inputText }]);
+			setTodos((prevTodos: Todo[]) => [...prevTodos, { id: uuidv4(), text: inputText }]);
 			setInputText("");
 		} else {
 			alert("Hey type todo first dude!");
@@ -30,17 +31,32 @@ export default function Todos() {
 	};
 
 	return (
-		<main>
-			<input type="text" name="todo" id="todoInput" value={inputText} onChange={handleInputChange} />
-			<button onClick={addTodo}>Add Todo</button>
-			<button onClick={clearTodos}>Clear Todos</button>
-			{todos.map((todo: Todo) => {
-				return (
-					<ul>
-						<li key={todo.id}>{todo.text}</li>
-					</ul>
-				);
-			})}
+		<main className="flex flex-col gap-2">
+			<div className="flex flex-row gap-2">
+				<input
+					className="border-2 rounded"
+					type="text"
+					name="todo"
+					id="todoInput"
+					value={inputText}
+					onChange={handleInputChange}
+				/>
+				<button className="bg-black text-white" onClick={addTodo}>
+					Add Todo
+				</button>
+				<button className="bg-black text-white" onClick={clearTodos}>
+					Clear Todos
+				</button>
+			</div>
+			<ul className="flex flex-col gap-2">
+				{todos.map((todo: Todo) => {
+					return (
+						<li className="gap-2" key={todo.id}>
+							<Todo text={todo.text} id={todo.id} />
+						</li>
+					);
+				})}
+			</ul>
 		</main>
 	);
 }
